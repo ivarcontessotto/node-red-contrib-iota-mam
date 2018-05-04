f35598cafcf70a01a931efa87e4a1a4bb6f404c4
Alexander <alexander@andlabs.eu>
2018-05-04 02:27:34 +0200

    Initial commit

---
+++ mam.js
@@ -0,0 +1,28 @@
+const IOTA = require('iota.lib.js');
+const iota = new IOTA({ provider: 'http://node04.nodeblie.de:14265/ ' })
+const MAM = require('./mam.node.js');
+
+module.exports = function(RED) {
+    function LowerCaseNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        let mamState = MAM.init(iota)
+
+        // trigger for mam update
+        // some random msg input
+        node.on('input', function(msg) {
+            // msg.payload = msg.payload.toLowerCase();
+            // node.send(msg);
+            var self = this;
+            console.log(msg)
+            // let rootVal = "TSC9BIOTHQRDLDDALNNDWWXCSDNBCJJIAW9TRRWDUSWKW9B9AJ9O9BIIWIVDYLKHYPNKHZXTYVL9PIPFJ"
+            let resp = MAM.fetch(msg.payload, 'public', null, function(data) {
+        				let json = JSON.parse(iota.utils.fromTrytes(data));
+                msg.payload = JSON.stringify(json);
+                console.log("console "+ msg.payload)
+                self.send(msg);
+        		});
+        });
+    }
+    RED.nodes.registerType("mam",LowerCaseNode);
+}
